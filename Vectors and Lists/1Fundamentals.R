12b1b465e3e1ab10c4a21bd1afe44353aa7b5d39
Naman <naman.chachan@mitwpu.edu.in>
2025-03-09 23:48:21 +0530

    First

---
+++ Vectors and Lists/1Fundamentals.R
@@ -0,0 +1,15 @@
+#Vector(R) = A group of data elements of same type stored in a sequence
+#Vector made by combine function [c(a1, a2, a3)]
+first_variable <- "Hello naman"
+second_variable <- 46
+vec_1 <- c(22,334,112,33)
+vec_2 <-c(2:10)
+
+vec_1
+vec_2
+
+x <- c(1,2,3)
+names(x) <- c("a", "b", "c")
+x
+x[2]
+x["b"]
---
+++ Vectors and Lists/Lists.R
@@ -0,0 +1,15 @@
+#Lists are different from atomic vectors because their elements can be of any type—including 
+#characters, integers, and logical values. Lists can even contain other lists, matrices, vectors, or data frames. 
+
+list1 = list("a", 1L,2.3, TRUE )
+
+#As I learned, lists can contain other lists. Run the following code to create a list inside a list
+list(list(list(1,2,3)))
+
+str(list1)   #To find out what types of elements a list contains, use the str() function.
+
+#List elements, like vector elements, can be named when you create them with the list() function
+list('Chicago' = 1, 'New York' = 2, 'Los Angeles' = 3)
+
+
+#You can create two types of vectors in R: atomic vectors and lists
